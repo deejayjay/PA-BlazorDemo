1e039236f2c1beb93d5e80e8cc9e0fafdfdb6911
DeeJayJay <deejayjay@zoho.com>
2024-03-13 07:51:34 -0600

    Adds the project to the repo.

---
+++ BlazorDemo/Data/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorDemo.Data
+{
+    public class Student
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public void ResetToDefault()
+        { 
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Age = 0;
+        }
+    }
+}
---
+++ BlazorDemo/Program.cs
@@ -0,0 +1,33 @@
+using BlazorDemo.Data;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor()
+    .AddHubOptions(o => o.MaximumReceiveMessageSize = 1024 * 1024); // Increases max file upload size to 1024KB
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddRadzenComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
